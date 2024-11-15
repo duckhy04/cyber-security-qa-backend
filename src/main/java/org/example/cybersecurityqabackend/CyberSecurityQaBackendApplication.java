5261aa5255538fde11b9a04d7ee6d1a4336c51c0
duckhy04 <nguynduchuy12@gmail.com>
2024-11-16 00:01:30 +0700

    Update config database

---
+++ src/main/java/org/example/cybersecurityqabackend/CyberSecurityQaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.cybersecurityqabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberSecurityQaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberSecurityQaBackendApplication.class, args);
+    }
+
+}
