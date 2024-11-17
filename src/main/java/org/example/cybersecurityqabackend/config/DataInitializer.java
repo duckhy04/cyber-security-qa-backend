fdf9ed40db17b7840bccc976a17c066f9c1f44c5
duckhy04 <nguynduchuy12@gmail.com>
2024-11-17 20:47:41 +0700

    Refactor DataInitializer

--- src/main/java/org/example/cybersecurityqabackend/config/DataInitializer.java
+++ src/main/java/org/example/cybersecurityqabackend/config/DataInitializer.java
@@ -1,7 +1,6 @@
 package org.example.cybersecurityqabackend.config;
 
 import lombok.AllArgsConstructor;
-import org.example.cybersecurityqabackend.entity.Role;
 import org.example.cybersecurityqabackend.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,25 +10,7 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class DataInitializer implements CommandLineRunner {
 
-    private final RoleRepository roleRepository;
-
     @Override
     public void run(String... args) throws Exception {
-
-        // Thêm vai trò Admin nếu chưa có
-        if (!roleRepository.existsByName("ADMIN")) {
-            Role adminRole = new Role();
-            adminRole.setName("ADMIN");
-            adminRole.setDescription("Administrator role with all privileges");
-            roleRepository.save(adminRole);
-        }
-
-        // Thêm vai trò User nếu chưa có
-        if (!roleRepository.existsByName("USER")) {
-            Role userRole = new Role();
-            userRole.setName("USER");
-            userRole.setDescription("Regular user role with basic privileges");
-            roleRepository.save(userRole);
-        }
     }
 }
