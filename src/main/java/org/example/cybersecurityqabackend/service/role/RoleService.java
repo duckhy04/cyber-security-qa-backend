beac80dbf434a1f980db7a33927525635983891c
duckhy04 <nguynduchuy12@gmail.com>
2024-11-17 19:56:01 +0700

    Add service

---
+++ src/main/java/org/example/cybersecurityqabackend/service/role/RoleService.java
@@ -0,0 +1,4 @@
+package org.example.cybersecurityqabackend.service.role;
+
+public interface RoleService {
+}
---
+++ src/main/java/org/example/cybersecurityqabackend/service/role/RoleServiceImpl.java
@@ -0,0 +1,17 @@
+package org.example.cybersecurityqabackend.service.role;
+
+import org.example.cybersecurityqabackend.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+
+}
