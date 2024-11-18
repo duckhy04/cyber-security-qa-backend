635e1a360ed8ea64b59e2ca9852d87da02a7f157
duckhy04 <nguynduchuy12@gmail.com>
2024-11-18 13:02:41 +0700

    Refactor code

--- src/main/java/org/example/cybersecurityqabackend/service/role/RoleServiceImpl.java
+++ src/main/java/org/example/cybersecurityqabackend/service/role/RoleServiceImpl.java
@@ -1,33 +1,28 @@
 package org.example.cybersecurityqabackend.service.role;
 
 import jakarta.annotation.PostConstruct;
-import lombok.AllArgsConstructor;
 import org.example.cybersecurityqabackend.entity.Role;
 import org.example.cybersecurityqabackend.repository.RoleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class RoleServiceImpl implements RoleService {
     private final RoleRepository roleRepository;
 
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
     @PostConstruct
     public void init() {
-        // Thêm role Admin nếu chưa có
-        if (!roleRepository.existsByName("ADMIN")) {
-            Role adminRole = new Role();
-            adminRole.setName("ADMIN");
-            adminRole.setDescription("Administrator role with all privileges");
-            roleRepository.save(adminRole);
-        }
+        if (roleRepository.count() == 0) {
+            Role roleAdmin = new Role();
+            roleAdmin.setName("ROLE_ADMIN");
+            roleRepository.save(roleAdmin);
 
-        // Thêm role User nếu chưa có
-        if (!roleRepository.existsByName("USER")) {
-            Role userRole = new Role();
-            userRole.setName("USER");
-            userRole.setDescription("Regular user role with basic privileges");
-            roleRepository.save(userRole);
+            Role roleUser = new Role();
+            roleUser.setName("ROLE_USER");
+            roleRepository.save(roleUser);
         }
     }
 }
