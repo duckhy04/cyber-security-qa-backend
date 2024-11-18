4edff873ccd6625fb9ccad615bc56bf281b8b6cb
duckhy04 <nguynduchuy12@gmail.com>
2024-11-18 13:49:03 +0700

    Add userinfo for response login

--- src/main/java/org/example/cybersecurityqabackend/controller/AuthController.java
+++ src/main/java/org/example/cybersecurityqabackend/controller/AuthController.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import org.example.cybersecurityqabackend.dto.JwtAuthResponse;
 import org.example.cybersecurityqabackend.dto.LoginDto;
 import org.example.cybersecurityqabackend.dto.RegisterDto;
+import org.example.cybersecurityqabackend.dto.UserDto;
+import org.example.cybersecurityqabackend.entity.User;
 import org.example.cybersecurityqabackend.service.auth.AuthService;
+import org.example.cybersecurityqabackend.service.user.UserSerivce;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,14 +17,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private AuthService authService;
+    private final UserSerivce userSerivce;
+    private final AuthService authService;
 
     @PostMapping("/login")
     public ResponseEntity<JwtAuthResponse> login(@RequestBody LoginDto loginDto){
         String token = authService.login(loginDto);
 
+        // Lấy thông tin người dùng
+        User user = userSerivce.findByUsername(loginDto.getUsernameOrEmail(), loginDto.getUsernameOrEmail());
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setUsername(user.getUsername());
+        userDto.setEmail(user.getEmail());
+
         JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
         jwtAuthResponse.setAccessToken(token);
+        jwtAuthResponse.setUser(userDto);
 
         return new ResponseEntity<>(jwtAuthResponse, HttpStatus.OK);
     }
