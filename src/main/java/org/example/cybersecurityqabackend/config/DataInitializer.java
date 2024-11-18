94dd5bd6c787b6e416c4ae38f3d35d24428a6a2e
duckhy04 <nguynduchuy12@gmail.com>
2024-11-18 13:01:51 +0700

    Refactor code

--- src/main/java/org/example/cybersecurityqabackend/config/DataInitializer.java
+++ src/main/java/org/example/cybersecurityqabackend/config/DataInitializer.java
@@ -1,12 +1,9 @@
 package org.example.cybersecurityqabackend.config;
 
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class DataInitializer implements CommandLineRunner {
 
     @Override
