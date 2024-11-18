73ef3ecf3e5b3d33b505a5bba7df0aba850ad1e0
duckhy04 <nguynduchuy12@gmail.com>
2024-11-18 13:04:03 +0700

    Refactor code

--- src/main/java/org/example/cybersecurityqabackend/repository/UserRepository.java
+++ src/main/java/org/example/cybersecurityqabackend/repository/UserRepository.java
@@ -9,6 +9,4 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsernameOrEmail(String username, String email);
-    boolean existsByUsername(String username);
-    boolean existsByEmail(String email);
 }
