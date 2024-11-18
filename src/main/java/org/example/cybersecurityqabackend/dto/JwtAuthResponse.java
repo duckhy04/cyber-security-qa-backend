fcb0d8444531c5c6ace540e1ab2cbc0acd5fc5b7
duckhy04 <nguynduchuy12@gmail.com>
2024-11-18 13:49:15 +0700

    Update JwtAuthResponse

--- src/main/java/org/example/cybersecurityqabackend/dto/JwtAuthResponse.java
+++ src/main/java/org/example/cybersecurityqabackend/dto/JwtAuthResponse.java
@@ -5,5 +5,6 @@ import lombok.Data;
 @Data
 public class JwtAuthResponse {
     private String accessToken;
-    private String tokenType = "Bearer";
+    private UserDto user;
+//    private String tokenType = "Bearer";
 }
