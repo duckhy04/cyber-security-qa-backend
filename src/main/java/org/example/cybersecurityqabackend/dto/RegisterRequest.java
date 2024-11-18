1f9cdee0f055ad5335ec2c67818c7e413bee01ba
duckhy04 <nguynduchuy12@gmail.com>
2024-11-19 02:59:44 +0700

    Update RegisterRequest.java

--- src/main/java/org/example/cybersecurityqabackend/dto/RegisterRequest.java
+++ src/main/java/org/example/cybersecurityqabackend/dto/RegisterRequest.java
@@ -3,7 +3,7 @@ package org.example.cybersecurityqabackend.dto;
 import lombok.Data;
 
 @Data
-public class RegisterDto {
+public class RegisterRequest {
     private String username;
     private String email;
     private String password;
