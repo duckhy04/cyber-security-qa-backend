0f8e9cb5ae5b4ffd3f918dbac30e4b437080d213
duckhy04 <nguynduchuy12@gmail.com>
2024-11-18 13:02:23 +0700

    Refactor code

--- src/main/java/org/example/cybersecurityqabackend/entity/Role.java
+++ src/main/java/org/example/cybersecurityqabackend/entity/Role.java
@@ -1,38 +1,16 @@
 package org.example.cybersecurityqabackend.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
-
-import java.util.Date;
-import java.util.Set;
+import lombok.*;
 
 @Data
 @Entity
 @Table(name = "roles")
 public class Role {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false, length = 50)
     private String name;
-
-    @Column(name = "description")
-    private String description;
-
-    @ManyToMany(mappedBy = "roles")
-    private Set<User> users;
-
-    @Column(name = "created_at", nullable = false, updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdAt = new Date();
-
-    @Column(name = "updated_at")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date updatedAt = new Date();
-
-    @PreUpdate()
-    public void preUpdate() {
-        updatedAt = new Date();
-    }
 }
