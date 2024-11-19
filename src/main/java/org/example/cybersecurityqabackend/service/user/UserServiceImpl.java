c9f9a5aaee7f990818e158af70e3cb86e8ea983f
duckhy04 <nguynduchuy12@gmail.com>
2024-11-19 13:33:58 +0700

    Update UserServiceImpl.java

--- src/main/java/org/example/cybersecurityqabackend/service/user/UserServiceImpl.java
+++ src/main/java/org/example/cybersecurityqabackend/service/user/UserServiceImpl.java
@@ -1,13 +1,12 @@
 package org.example.cybersecurityqabackend.service.user;
 
 import org.example.cybersecurityqabackend.entity.User;
+import org.example.cybersecurityqabackend.exception.ResourceNotFoundException;
 import org.example.cybersecurityqabackend.repository.UserRepository;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserSerivce{
-
 
     private final UserRepository userRepository;
 
@@ -18,6 +17,6 @@ public class UserServiceImpl implements UserSerivce{
     @Override
     public User findByUsername(String username, String email) {
         return userRepository.findByUsernameOrEmail(username, email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 }
