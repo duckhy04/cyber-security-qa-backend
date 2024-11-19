6ab04bbfb76186949f7a941f4f6c1a185621ebb1
duckhy04 <nguynduchuy12@gmail.com>
2024-11-19 13:33:29 +0700

    Update JwtAuthenticationEntryPoint.java

--- src/main/java/org/example/cybersecurityqabackend/exception/JwtAuthenticationEntryPoint.java
+++ src/main/java/org/example/cybersecurityqabackend/exception/JwtAuthenticationEntryPoint.java
@@ -14,6 +14,9 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("{\"code\": \"INVALID_CREDENTIALS\", \"message\": \"Incorrect login information.\"}");
     }
 }
